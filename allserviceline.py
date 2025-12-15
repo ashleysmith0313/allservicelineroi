6ad486a6fc0e14823439b63880818a4136309bc3
ashleysmith0313 <ashleysmith0313@yahoo.com>
2025-12-15 17:03:14 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- allserviceline.py
+++ allserviceline.py
@@ -590,8 +590,6 @@ if st.button("Download Analyst Report (PDF)"):
         st.download_button(
             label="Download PDF",
             data=pdf_bytes,
-            file_name="{}_analyst_report.pdf".format(service_name.replace(' ', '_').lower()),
-            mime="application/pdf",
-        ).lower()}_analyst_report.pdf",
+            file_name="{}_analyst_report.pdf".format(service_name.replace(' ', '_').replace('/', '_').lower()),
             mime="application/pdf",
         )
